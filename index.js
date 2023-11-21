b2f1d2e55e623a1f11997ff416f2e5834931a584
JeremiahID <jeremiahefe.je@gmail.com>
2023-11-21 09:26:27 +0100

    Java SCript Added for the hamburger

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
+/*this is the js for the hambuger drop down*/
 const menu = document.querySelector('#menu');
-const hiddenMenu = document.querySelector('.hiddenSection')
+const hiddenMenu = document.querySelector('.hiddenSection');
 
 menu.addEventListener('click' , () => {
     hiddenMenu.classList.toggle("show");
     })
 
+
+    /*this is the js for the sectionOne display none */
 const SectionOne = document.querySelector('.sectionOne')
-    
+
 menu.addEventListener('click' , () => {
     SectionOne.classList.toggle("sectionOneShow");
      })
-    
+
+/*this is the js for the cart drop down*/
+
+const shoppingCart = document.querySelector('#cart');
+const ShopBags = document.document.querySelector('.shopBag');
+
+shoppingCart.addEventListener('click' , () => {
+    ShopBags.classList.toggle('shop')
+})
+
+const SectionOneCart = document.querySelector('.sectionOne')
+
+shoppingCart.addEventListener('click' , () => {
+    SectionOneCart.classList.toggle("sectionOneShow");
+     })
 
 
-// var menu = document.getElementById("menu");
-// menu.classList.toggle("show");
-// 
