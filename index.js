70988f059f278f0ce77ef7255597cc556bc15fb7
JeremiahID <jeremiahefe.je@gmail.com>
2023-11-22 18:29:38 +0100

    index Update on java script

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ Menu.addEventListener('click' , () => {
     SectionTwo.classList.toggle("sectionOneShow");
     SectionFour.classList.toggle("sectionOneShow");
     Cart.classList.toggle("sectionOneShow")
-   
 })
 
 
