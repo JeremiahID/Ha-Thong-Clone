bad03324602ca199ebe4086f69fae220c7cac01a
JeremiahID <jeremiahefe.je@gmail.com>
2024-01-22 12:09:41 +0100

    index updates

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 /*this is the js for the hambuger drop down*/
 const menu = document.querySelector('#menu');
-const hiddenMenu = document.querySelector('.hiddenSection');
-
-menu.addEventListener('click' , () => {
-    hiddenMenu.classList.toggle("show");
-})
+const Cart = document.querySelector('#cart')
+// const hiddenMenu = document.querySelector('.hiddenSection');
 
 
-/*this is the js for the sectionOne display none */
-const Menu =  document.querySelector('#menu')
-const SectionOne = document.querySelector('.sectionOne');
-const SectionTwo = document.querySelector('.sectionTwo');
-const SectionFour = document.querySelector('.sectionFour');
-const Cart  =  document.querySelector('.shopBag')
 
 
-Menu.addEventListener('click' , () => {
-    SectionOne.classList.toggle("sectionOneShow");
-    SectionTwo.classList.toggle("sectionOneShow");
-    SectionFour.classList.toggle("sectionOneShow");
-    Cart.classList.toggle("sectionOneShow")
-})
+
+// menu.addEventListener('click' , () => {
+//     hiddenMenu.classList.toggle("show");
+// })
+
+
+// /*this is the js for the sectionOne display none */
+// const Menu =  document.querySelector('#menu')
+// const SectionOne = document.querySelector('.sectionOne');
+// const SectionTwo = document.querySelector('.sectionTwo');
+// const SectionFour = document.querySelector('.sectionFour');
+// const Cart  =  document.querySelector('.shopBag')
+
+
+// Menu.addEventListener('click' , () => {
+//     SectionOne.classList.toggle("sectionOneShow");
+//     SectionTwo.classList.toggle("sectionOneShow");
+//     SectionFour.classList.toggle("sectionOneShow");
+//     Cart.classList.toggle("sectionOneShow")
+// })
 
 
 
